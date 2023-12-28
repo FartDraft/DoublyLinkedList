3a9af4722261a70f87ef089778e5db966b4fa9c7
”FartDraft” <fartdraft@gmail.com>
2023-12-28 13:47:48 +0300

    Basic

--- src/inc/DoublyLinkedList.hh
+++ src/inc/DoublyLinkedList.hh
@@ -1,1 +1,23 @@
 #pragma once
+
+#include <cstdint>
+#include <vector>
+
+#define SIZE 64
+
+template <typename T, typename S = unsigned>
+class DoublyLinkedList {
+  public:
+    struct node {
+        node* prev;
+        T value;
+        node* next;
+    };
+
+    uint64_t length = 0;
+    S size = SIZE;
+    node *head{}, *tail{};
+    std::vector<node*> refs = {head, tail}; // {head, SIZE, 2*SIZE, ..., n*SIZE, head}
+
+    DoublyLinkedList(S size = SIZE) : size{size} {}
+};
--- src/inc/test/DoublyLinkedList.hh
+++ src/inc/test/DoublyLinkedList.hh
@@ -1,3 +1,15 @@
 #pragma once
 #include <gtest/gtest.h>
 #include "../DoublyLinkedList.hh"
+
+TEST(Constructor, Empty) {
+    DoublyLinkedList<int> list;
+
+    ASSERT_EQ(SIZE, list.size);
+}
+
+TEST(Constructor, Size) {
+    DoublyLinkedList<int> list{10};
+
+    ASSERT_EQ(10, list.size);
+}
