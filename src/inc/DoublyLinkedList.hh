9afa2d49eb32961ec03bf3b1a05efbe204178ef3
”FartDraft” <fartdraft@gmail.com>
2023-12-28 14:55:19 +0300

    +push_tail +ostream

--- src/inc/DoublyLinkedList.hh
+++ src/inc/DoublyLinkedList.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <iostream>
 #include <vector>
 
 #define SIZE 64
@@ -17,7 +18,31 @@ class DoublyLinkedList {
     uint64_t length = 0;
     S size = SIZE;
     node *head{}, *tail{};
-    std::vector<node*> refs = {head, tail}; // {head, SIZE, 2*SIZE, ..., n*SIZE, head}
+    std::vector<node*> refs = {head, tail};
 
-    DoublyLinkedList(S size = SIZE) : size{size} {}
+    explicit constexpr DoublyLinkedList(S size = SIZE) : size{size} {}
+
+    constexpr node*
+    push_tail(const T& value) {
+        ++length;
+        if (head == nullptr) {
+            return refs.front() = refs.back() = head = tail = new node{nullptr, value, nullptr};
+        }
+        return refs.back() = tail = tail->next = new node{tail, value, nullptr};
+    }
+
+    friend std::ostream&
+    operator<<(std::ostream& os, const DoublyLinkedList<T>& list) {
+        os << "head -> ";
+        if (list.head == nullptr) {
+            os << "nullptr";
+        } else {
+            os << list.head->value;
+            for (node* curr = list.head->next; curr != nullptr; curr = curr->next) {
+                os << " <-> " << curr->value;
+            }
+        }
+        os << " <- tail" << std::endl;
+        return os;
+    }
 };
--- src/inc/test/DoublyLinkedList.hh
+++ src/inc/test/DoublyLinkedList.hh
@@ -13,3 +13,37 @@ TEST(Constructor, Size) {
 
     ASSERT_EQ(10, list.size);
 }
+
+TEST(Stream, Empty) {
+    DoublyLinkedList<char> list;
+
+    std::cout << list;
+}
+
+TEST(Stream, One) {
+    DoublyLinkedList<char> list;
+
+    list.push_tail('a');
+    std::cout << list;
+
+    ASSERT_EQ(1, list.length);
+    ASSERT_EQ(list.head, list.refs[0]);
+    ASSERT_EQ(list.tail, list.refs[1]);
+    ASSERT_EQ(list.head, list.tail);
+}
+
+TEST(Stream, Many) {
+    DoublyLinkedList<char> list;
+
+    list.push_tail('a');
+    list.push_tail('b');
+    list.push_tail('c');
+    std::cout << list;
+
+    ASSERT_EQ(3, list.length);
+    ASSERT_EQ('a', list.head->value);
+    ASSERT_EQ('b', list.head->next->value);
+    ASSERT_EQ('c', list.tail->value);
+    ASSERT_EQ(list.head, list.refs[0]);
+    ASSERT_EQ(list.tail, list.refs[1]);
+}
